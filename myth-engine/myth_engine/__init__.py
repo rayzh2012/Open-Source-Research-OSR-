41e3cd1abbb5738be367929162ba67413ce058ef
Hang Wu <rayzh2013@gmail.com>
2026-08-20 01:41:36 -0400

    feat(myth-engine): expose core API

---
+++ myth-engine/myth_engine/__init__.py
@@ -0,0 +1,21 @@
+from .core import (
+    KeywordAutomaton,
+    MythDB,
+    SearchHit,
+    canonical_text,
+    merkle_root,
+    normalize_text,
+    qgrams,
+    sha256_text,
+)
+
+__all__ = [
+    "KeywordAutomaton",
+    "MythDB",
+    "SearchHit",
+    "canonical_text",
+    "merkle_root",
+    "normalize_text",
+    "qgrams",
+    "sha256_text",
+]
